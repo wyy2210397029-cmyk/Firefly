343790fb0ae7757198240ea2f32a9c02528955c0
Fishction <wyy2210397029@gmail.com>
2026-06-05 21:57:50 +0800

    更新字体配置

--- src/config/fontConfig.ts
+++ src/config/fontConfig.ts
@@ -5,7 +5,7 @@ export const fontConfig = {
 	// 是否预加载字体文件
 	preload: true,
 	// 当前选择的字体，支持多个字体组合
-	selected: ["misans-regular"],
+	selected: ["zen-maru-gothic"],
 
 	// 字体列表
 	// 推荐使用可靠的 CDN 服务商提供的字体链接，它天然做了按需分片加载，且性能较好
