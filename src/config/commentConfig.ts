87d05ed3b77ff058833287ea11c335707a3f5958
Fishction <wyy2210397029@gmail.com>
2026-06-05 19:39:02 +0800

    更新 Twikoo 地址

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -6,7 +6,7 @@ export const commentConfig: CommentConfig = {
 
 	//twikoo评论系统配置
 	twikoo: {
-		envId: "https://firefly-blue.vercel.app",
+		envId: "https://twikoo-vercel-omega-two.vercel.app",
 		// 设置 Twikoo 评论系统语言
 		lang: "zh-CN",
 		// 是否启用文章访问量统计功能
