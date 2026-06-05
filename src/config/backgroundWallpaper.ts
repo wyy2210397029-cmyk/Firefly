23a5587dd836db8a5f2f76c7d8586a94c8a27d0c
Fishction <wyy2210397029@gmail.com>
2026-06-05 18:09:21 +0800

    更新博客

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -64,17 +64,14 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Yuuuuuuuuooooo",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
 			subtitle: [
-				"In Reddened Chrysalis, I Once Rest",
-				"From Shattered Sky, I Free Fall",
-				"Amidst Silenced Stars, I Deep Sleep",
-				"Upon Lighted Fyrefly, I Soon Gaze",
-				"From Undreamt Night, I Thence Shine",
-				"In Finalized Morrow, I Full Bloom",
+				"Everything that happens is from now on.",
+				"Crossing The Ocean.",
+				"The dragon had eaten the boxer.",
 			],
 			// 主页横幅副标题字体大小
 			subtitleSize: "1.5rem",
